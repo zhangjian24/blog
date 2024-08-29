e3b639825d8c2093ed8550c40303a51d17fde5a7
giteaadmin <751772734@qq.com>
2024-08-29 15:51:18 +0800

    theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,26 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import rehypeToc from '@jsdevtools/rehype-toc'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 import mdx from "@astrojs/mdx";
+import expressiveCode from "astro-expressive-code"; 
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    expressiveCode({
+    themes:["catppuccin-latte","monokai"], 
+    plugins: [pluginLineNumbers()]
+  }),tailwind(), mdx()],
   markdown: {
 		rehypePlugins: [
       rehypeSlug,
       [rehypeAutolinkHeadings, { behavior: 'prepend' }],
-			[rehypeToc, { headings: ['h1','h2', 'h3'] }],
 		],
 	},
 });
